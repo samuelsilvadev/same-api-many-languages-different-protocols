0c68a4ded2a08529bfca1eb8d312ed0ef19e9006
Samuel Silva <samuelsilvawb@gmail.com>
2025-03-23 15:38:17 +0000

    fix: wrong mapping type for email

--- basic-commerce-api-python/src/models.py
+++ basic-commerce-api-python/src/models.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from .db import Base
-from pydantic import EmailStr
+
 
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    email: Mapped[EmailStr] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(nullable=False)
